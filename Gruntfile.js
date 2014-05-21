75315c1b46689312e78995974c43c7ce6f874bb4
Vilson Vieira <vilson@void.cc>
2014-05-21 09:46:35 -0300

    now we are synced with KIELER/Klay repository, grunt tasks are OK and there is better docs on readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,31 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     curl: {
-      'klay_layered_js_nightly_latest.zip': 'http://rtsys.informatik.uni-kiel.de/~kieler/files/nightly/klayjs/klay_layered_js_nightly_latest.zip'
+      'klay_layered_js_webworkerlinker_nightly_latest.zip': 'http://rtsys.informatik.uni-kiel.de/~kieler/files/nightly/klayjs/klay_layered_js_webworkerlinker_nightly_latest.zip'
     },
     unzip: {
-      'klay': 'klay_layered_js_nightly_latest.zip'
+      'klay': 'klay_layered_js_webworkerlinker_nightly_latest.zip'
+    },
+    rename: {
+      options: {
+        ignore: true
+      },
+      moveThis: {
+        src: 'klay/klayjs_worker.js',
+        dest: 'klay-worker.js'
+      }
     },
     clean: {
-      klay_package: 'klay_layered_js_nightly_latest.zip'
+      klay_package: 'klay_layered_js_webworkerlinker_nightly_latest.zip',
+      temp_dir: 'klay'
     }
   });
 
   grunt.loadNpmTasks('grunt-curl');
   grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-rename');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['curl', 'unzip', 'clean']);
+  grunt.registerTask('default', ['curl', 'unzip', 'rename', 'clean']);
   
 };
